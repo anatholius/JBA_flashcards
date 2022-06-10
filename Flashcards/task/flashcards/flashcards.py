22eec264b614c0fd74b6671a5f91039145a394b2
Anatol Derbisz <anatholius@gmail.com>
2022-06-10 22:19:12 +0200

    `Flashcards`/`Work on project`::`Stage 4/7`: A good stack ✅

--- Flashcards/task/flashcards/flashcards.py
+++ Flashcards/task/flashcards/flashcards.py
@@ -1,18 +1,31 @@
-from collections import deque
-
-
 class Flashcard:
 
     def __init__(self, count: int):
         self.count = count
-        self.terms = {}
+        self.terms = dict()
 
     def define(self):
+        term = None
+        definition = None
         while len(self.terms) < self.count:
             no = len(self.terms) + 1
-            term = input(f'The term for card #{no}:\n')
-            definition = input(f'The definition for card #{no}:\n')
-            self.terms[term] = definition
+            if not term:
+                term = input(f'The term for card #{no}:\n')
+            if term in self.terms.keys():
+                term = input(f'The term "{term}" already exists. Try again:\n')
+                continue
+            if not definition:
+                definition = input(f'The definition for card #{no}:\n')
+            if definition in self.terms.values():
+                definition = input(
+                    f'The definition "{definition}" already exists. Try '
+                    f'again:\n')
+                continue
+            if term and definition:
+                self.terms[term] = definition
+            term = None
+            definition = None
+
         return self
 
     def ask(self):
@@ -22,7 +35,17 @@ class Flashcard:
             if answer == definition:
                 print('Correct!')
             else:
-                print(f'Wrong. The right answer is "{definition}"')
+                if answer in self.terms.values():
+                    correct_answer = list(self.terms.keys())[
+                        list(self.terms.values()).index(answer)
+                    ]
+                    correct_term = self.terms[correct_answer]
+                    print(
+                        f'Wrong. The right answer is "{definition}", '
+                        f'but your definition is correct for "'
+                        f'{correct_answer}".')
+                else:
+                    print(f'Wrong. The right answer is "{definition}"')
 
 
 flash = Flashcard(int(input('Input the number of cards:\n'))).define()
