7c3b3957e6489ff808a6ec9f8218f9c8d685f9f8
Anatol Derbisz <anatholius@gmail.com>
2022-06-10 20:39:45 +0200

    `Flashcards`/`Work on project`::`Stage 3/7`: Nested lists ⌛ first try

--- Flashcards/task/flashcards/flashcards.py
+++ Flashcards/task/flashcards/flashcards.py
@@ -1,6 +1,31 @@
-term = input()
-definition = input()
-answer = input(term)
-print('Your answer is {}'.format(
-    'right!' if definition == answer else 'wrong...'
-))
+from collections import deque
+
+
+class Flashcard:
+
+    def __init__(self, count):
+        self.count = count
+        self.terms = {}
+
+    def define(self):
+        defs = set(list([('a', '1'), ('s', '2')]))
+        while len(self.terms) < self.count:
+            item = defs.pop()
+            self.terms[item[0]] = item[1]
+        return self
+
+    def ask(self):
+        quiz = deque(set(self.terms.items()))
+        while len(quiz) > 0:
+            item = quiz[0]
+            if input(f'Type definition for "{item[0]}": ') == item[1]:
+                print('Correct')
+                quiz.popleft()
+            else:
+                print(f'Wrong. The right answer is "{item[1]}"')
+
+        print('No more cards!')
+
+
+flash = Flashcard(2).define()
+flash.ask()
