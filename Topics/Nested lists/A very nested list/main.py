866142bf451a045a392a7a3e2f1572a52cfdb09c
Anatol Derbisz <anatholius@gmail.com>
2022-06-12 20:39:41 +0200

    `Code quality`/`Testing and debugging`::`Testing user input`: Updated `Topics` during daily problem ⌛✅

---
+++ Topics/Nested lists/A very nested list/main.py
@@ -0,0 +1,3 @@
+str_1 = input()
+str_2 = input()
+str_3 = input()
---
+++ Topics/Nested lists/Word list/main.py
@@ -0,0 +1,5 @@
+text = [["Glitch", "is", "a", "minor", "problem", "that", "causes", "a", "temporary", "setback"],
+        ["Ephemeral", "lasts", "one", "day", "only"],
+        ["Accolade", "is", "an", "expression", "of", "praise"]]
+limit = int(input())
+print([w for subtext in text for w in subtext if len(w) <= limit])
