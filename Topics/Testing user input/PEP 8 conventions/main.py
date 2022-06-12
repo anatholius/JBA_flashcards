26d8ee65c0ed9bdeef057a55136afc0e44e0bb29
Anatol Derbisz <anatholius@gmail.com>
2022-06-12 19:55:42 +0200

    `Code quality`/`Testing and debugging`::`Testing user input`: PEP 8 conventions ✅

--- Topics/Testing user input/PEP 8 conventions/main.py
+++ Topics/Testing user input/PEP 8 conventions/main.py
@@ -1,2 +1,10 @@
-def check_name(...):
-    ...
+def check_name(name):
+    if name in 'lOI':
+        print(
+            "Never use the characters 'l', 'O', or 'I' as single-character variable names")
+    elif name == name.lower():
+        print("It is a common variable")
+    elif name == name.upper():
+        print("It is a constant")
+    else:
+        print("You shouldn't use mixedCase")
