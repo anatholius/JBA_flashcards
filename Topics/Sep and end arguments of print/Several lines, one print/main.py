cc2d55e13d0f74c6f3c3b9dc30d7445ede0dcfcd
Anatol Derbisz <anatholius@gmail.com>
2022-06-13 04:41:21 +0200

    `Basics`/`Builtins`::`Sep and end arguments of print`: Several lines, one print ✅

--- Topics/Sep and end arguments of print/Several lines, one print/main.py
+++ Topics/Sep and end arguments of print/Several lines, one print/main.py
@@ -1,7 +1,6 @@
-line1 = "Night, square, apothecary, lantern,"
-line2 = "Its meaningless and pallid light."
-line3 = "Return a half a lifetime after – "
-line4 = "All will remain. A scapeless rite."
+line_1 = "Night, square, apothecary, lantern,"
+line_2 = "Its meaningless and pallid light."
+line_3 = "Return a half a lifetime after – "
+line_4 = "All will remain. A scapeless rite."
 
-# your one print() statement here
-print(...)
+print(line_1, line_2, line_3, line_4, sep='\n')
