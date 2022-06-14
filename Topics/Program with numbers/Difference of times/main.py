18485b581e083afe73f80a5da8d69d55057ca91f
Anatol Derbisz <anatholius@gmail.com>
2022-06-14 22:56:53 +0200

    `[Daily problem]` Difference of times ✅

--- Topics/Program with numbers/Difference of times/main.py
+++ Topics/Program with numbers/Difference of times/main.py
@@ -1,4 +1,4 @@
 x, y, z = int(input()), int(input()), int(input())
 a, b, c = int(input()), int(input()), int(input())
 i = 60
-print((a * i ** 2 + b * i + c) - (x * i ** 2 + y * i + z))
+print(a * i ** 2 + b * i + c - x * i ** 2 + y * i + z)
