a3b60994f17356977dcc09e705d2570dda8710d4
Anatol Derbisz <anatholius@gmail.com>
2022-06-15 00:25:16 +0200

    `Working with data`/`Collections`::`Lists`:`Sorting a list` -> Gaming task ✅

--- Topics/Sorting a list/Gaming task/main.py
+++ Topics/Sorting a list/Gaming task/main.py
@@ -1,4 +1,4 @@
-# the follwing line reads the list from the input, do not modify it, please
+# the following line reads the list from the input, do not modify it, please
 toys = input().split()
 
-# your code below
+print(sorted(toys, key=len))
