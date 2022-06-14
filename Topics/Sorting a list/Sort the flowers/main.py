337acc04e298df60adbee3234431a66f5e9528f6
Anatol Derbisz <anatholius@gmail.com>
2022-06-14 23:56:54 +0200

    `Working with data`/`Collections`::`Lists`:`Sorting a list` -> Sort the flowers ✅

--- Topics/Sorting a list/Sort the flowers/main.py
+++ Topics/Sorting a list/Sort the flowers/main.py
@@ -1,4 +1,4 @@
 # the following line creates a list from the input, do not modify it, please
 prices = [float(price) for price in input().split()]
 
-# your code below
+print(sorted(prices, reverse=True))
