05ed6ded1790f5d70dc6a08827efe0a3a4b144f5
Anatol Derbisz <anatholius@gmail.com>
2022-06-15 01:57:23 +0200

    `Working with data`/`Working with files`::`Context manager`: Error ✅

--- Topics/Context manager/Error/main.py
+++ Topics/Context manager/Error/main.py
@@ -1,5 +1,5 @@
 with open('years.txt', 'w', encoding='utf-8') as f:
-    for i in range(2010, 2020):
-        f.write(str(i) + " ")
-
-f.write('2020')
+    for i in range(int('2010'), int('2020')):
+        f.write(f'{i} ')
+        # print(i, file=f, end=' ')  # nice :) but longer :/
+    f.write('2020')
